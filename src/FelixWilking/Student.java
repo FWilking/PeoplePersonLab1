c515c17970cdd51fd289a85c9db379d17b673b59
chuang5877 <chuang5877@bths.edu>
2018-10-26 08:39:06 -0400

    studentstuff

--- src/FelixWilking/Student.java
+++ src/FelixWilking/Student.java
@@ -2,12 +2,14 @@ package FelixWilking;
 //yes
 
 public abstract class Student extends Person{
-    double GPA;
-    int OSIS;
-    int actScore;
-    public Student(double GPA, int OSIS,int actScore){
-        super();
-
+    private double GPA;
+    private int OSIS;
+    private int actScore;
+    public Student(String firstName, String lastName, double GPA, int OSIS,int actScore){
+        super(firstName,lastName);
+        this.GPA=GPA;
+        this.OSIS=OSIS;
+        this.actScore=actScore;
     }
     public Student(String firstName, String lastName) {
         super(firstName, lastName);
