6c92171e929909673fb5158f3f33962f45eb3c12
chuang5877 <chuang5877@bths.edu>
2018-10-26 08:33:38 -0400

    Student

---
+++ src/FelixWilking/Student.java
@@ -0,0 +1,14 @@
+package FelixWilking;
+
+public abstract class Student extends Person{
+    double GPA;
+    int OSIS;
+    int actScore;
+    public Student(double GPA, int OSIS,int actScore){
+        super();
+
+    }
+    public Student(String firstName, String lastName) {
+        super(firstName, lastName);
+    }
+}
